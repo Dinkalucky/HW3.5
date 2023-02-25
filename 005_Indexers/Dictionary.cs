c4d28f4f5c46568285fe47414205bf4bdd36e08a
Dina Nevhod <Dina_Nevhod@epam.com>
2023-02-25 18:35:09 +0100

    Task 3.5

---
+++ 005_Indexers/Dictionary.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Indexers
+{
+    class Dictionary
+    {
+        private string[] key = new string[5];
+        private string[] value = new string[5];
+        private string[] enWords = { "book", "pen", "sun", "apple", "table" };
+        private string[] ruWords = { "книга", "ручка", "солнце", "яблоко", "стол" };
+        private string[] uaWords = { "книга", "ручка", "сонце", "яблуко", "стіл" };
+
+        public Dictionary(string fromLang, string intoLang)
+        {
+            for (int i = 0; i < key.Length; i++)
+            {
+                if(fromLang == "EN")
+                {
+                    key[i] = enWords[i];
+                }
+                else if (fromLang == "RU")
+                {
+                    key[i] = ruWords[i];
+                }
+                else if (fromLang == "UA")
+                {
+                    key[i] = uaWords[i];
+                }
+
+                if (intoLang == "EN")
+                {
+                    value[i] = enWords[i];
+                }
+                else if (intoLang == "RU")
+                {
+                    value[i] = ruWords[i];
+                }
+                else if (intoLang == "UA")
+                {
+                    value[i] = uaWords[i];
+                }
+            }
+        }
+
+        public string this[string index]
+        {
+            get
+            {
+                for (int i = 0; i < key.Length; i++)
+                    if (key[i] == index)
+                        return key[i] + " - " + value[i];
+
+                return string.Format("{0} - немає перекладу для цього слова.", index);
+            }
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < key.Length)
+                    return key[index] + " - " + value[index];
+                else
+                    return "Спроба звернення за межі масиву.";
+            }
+        }
+    }
+}
---
+++ 005_Indexers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+// Індексатори.
+
+namespace Indexers
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            
+            string languageFromTranslation, languageIntoTranslation;
+
+            Console.WriteLine("З якої мови необхідно перекласти? (EN/RU/UA)");
+            languageFromTranslation = Console.ReadLine();
+
+            Console.WriteLine("На яку мову необхідно перекласти? (EN/RU/UA)");
+            languageIntoTranslation = Console.ReadLine();
+            Dictionary dictionary = new Dictionary(languageFromTranslation, languageIntoTranslation);
+            Console.WriteLine(dictionary["book"]);
+            Console.WriteLine(dictionary["стол"]);
+            Console.WriteLine(dictionary["pen"]);
+            Console.WriteLine(dictionary["стіл"]);
+            Console.WriteLine(dictionary["олівець"]);
+            Console.WriteLine(dictionary["яблоко"]);
+            Console.WriteLine(dictionary["sun"]);
+
+            Console.WriteLine(new string('-', 20));
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(dictionary[i]);
+            }
+                        
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
